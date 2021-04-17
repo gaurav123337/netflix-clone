f25770a101f6a05a4c7b3f8eadffe6d040a87665
Gaurav <gaurav.123337@gmail.com>
2021-04-18 00:03:45 +0530

    Added styles

--- src/Banner.js
+++ src/Banner.js
@@ -19,11 +19,14 @@ const Banner = () => {
 
   return (
     <header
-      className="banner"
+      className="banner1"
       style={{
         backgroundSize: "cover",
         backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
         backdropPosition: "center center",
+        color: 'white',
+        objectFit: 'contain',
+        height: '448px',
       }}
     >
       {/* Background image */}
